cf8e15c70b3b5701b90db476917619572e4cb8b2
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-09-08 14:04:08 +0800

    Create harbour-babymonitor-zh_CN.ts

---
+++ translations/harbour-babymonitor-zh_CN.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="39"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>active</source>
+        <translation>活动</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>inactive</source>
+        <translation>待命</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="38"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="42"/>
+        <source>Restore Default Settings</source>
+        <translation>恢复默认设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="65"/>
+        <source>Baby Monitor</source>
+        <translation>幼童监视器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="72"/>
+        <source>Warning: Alpha-quality code. Only suitable for testing. Please do not use this as a baby monitor! It is almost certainly going to break in unexpected ways.</source>
+        <translation>
+​警告: Alpha 测试阶段的代码，仅用于测试。请勿用作幼童监视器，否则将会出现未知错误。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="92"/>
+        <source>Sound Level Threshold</source>
+        <translation>声音大小范围</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="102"/>
+        <source>Sound Level</source>
+        <translation>声音大小</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="113"/>
+        <source>Trigger Cool Down Delay</source>
+        <translation>触发推迟时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>Trigger Threshold</source>
+        <translation>触发范围</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="134"/>
+        <source>Trigger</source>
+        <translation>触发</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="143"/>
+        <source>Dead Time</source>
+        <translation>截止时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="154"/>
+        <source>Remaining Dead Time</source>
+        <translation>剩余截止时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="163"/>
+        <source>Armed</source>
+        <translation>就绪</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="174"/>
+        <source>Phone Number</source>
+        <translation>电话号码</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="175"/>
+        <source>Please Enter Phone Number</source>
+        <translation>请输入电话号码</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-babymonitor</name>
+    <message>
+        <location filename="../qml/harbour-babymonitor.qml" line="44"/>
+        <location filename="../qml/harbour-babymonitor.qml" line="46"/>
+        <source>BabyMonitor triggered</source>
+        <translation>已触发幼童监视器</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-babymonitor.qml" line="45"/>
+        <location filename="../qml/harbour-babymonitor.qml" line="47"/>
+        <source>If the &apos;armed&apos; boxed had been checked, a call would have been placed.</source>
+        <translation>如果已就绪或者检测到装置，将会执行呼叫。</translation>
+    </message>
+</context>
+</TS>
