ecb7e4ed245497d7453797cd99de840a0ff1bfc1
Quentí <33203663+Quenty31@users.noreply.github.com>
2019-09-09 20:02:55 +0200

    Create harbour-babymonitor-fr.ts

---
+++ translations/harbour-babymonitor-fr.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" lang="fr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="39"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>active</source>
+        <translation>actif</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>inactive</source>
+        <translation>inactif</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="38"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="42"/>
+        <source>Restore Default Settings</source>
+        <translation>Rétablir les paramètres par défaut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="65"/>
+        <source>Baby Monitor</source>
+        <translation>Babyphone</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="72"/>
+        <source>Warning: Alpha-quality code. Only suitable for testing. Please do not use this as a baby monitor! It is almost certainly going to break in unexpected ways.</source>
+        <translation>Avertissement : code de qualité alpha. Convient uniquement pour les essais. Ne l'utilisez pas comme babyphone ! Il est presque sûr qu'il va se casser de façon inattendue.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="92"/>
+        <source>Sound Level Threshold</source>
+        <translation>Seuil de niveau sonore</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="102"/>
+        <source>Sound Level</source>
+        <translation>Niveau sonore</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="113"/>
+        <source>Trigger Cool Down Delay</source>
+        <translation>Déclenchement du délai de temporisation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>Trigger Threshold</source>
+        <translation>Seuil de déclenchement</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="134"/>
+        <source>Trigger</source>
+        <translation>Déclencheur</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="143"/>
+        <source>Dead Time</source>
+        <translation>Temps mort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="154"/>
+        <source>Remaining Dead Time</source>
+        <translation>Temps mort restant</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="163"/>
+        <source>Armed</source>
+        <translation>Activé</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="174"/>
+        <source>Phone Number</source>
+        <translation>Numéro de téléphone</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="175"/>
+        <source>Please Enter Phone Number</source>
+        <translation>Saisissez un numéro de téléphone</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-babymonitor</name>
+    <message>
+        <location filename="../qml/harbour-babymonitor.qml" line="44"/>
+        <location filename="../qml/harbour-babymonitor.qml" line="46"/>
+        <source>BabyMonitor triggered</source>
+        <translation>Babyphone activé</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-babymonitor.qml" line="45"/>
+        <location filename="../qml/harbour-babymonitor.qml" line="47"/>
+        <source>If the &apos;armed&apos; boxed had been checked, a call would have been placed.</source>
+        <translation>Si la case &apos;activé&apos; avait été cochée un appel aurait été passé.</translation>
+    </message>
+</context>
+</TS>
